44aae31d175bb798621e8ba1d7a9a4ab9f1de89d
AFlandin <fldnadam@gmail.com>
2026-02-04 23:16:32 +0100

    Simple main asserts

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <SDL3/SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 #include "Window.h"
 #include "InputHandler.h"
 #include "Idea.h"
@@ -19,8 +20,7 @@ int main(void) {
 	State st;
 
 	st.g_allocator = allocator_create(MiB(1));
-	if (!st.g_allocator)
-		return 1;
+	assert(st.g_allocator);
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
 		fprintf(stderr,
 			"Failed to init sdl3: %s\n", SDL_GetError());
@@ -29,8 +29,7 @@ int main(void) {
 	st.win = window_create(st.g_allocator, vec2i_of(1080, 720), "C Idea Handler");
 	st.inputh = inputhandler_create(st.g_allocator, st.win);
 	st.ideas = dynlist_create(sizeof(Idea), IDEAS_DLIST_ISIZE, (f_dynlist_free)idea_destroy);
-	if (!st.ideas)
-		return 1;
+	assert(st.ideas);
 	st.running = true;
 	while (st.running) {
 		Time now = time_now();
